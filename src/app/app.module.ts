db00264d96ca70768227e7dafdd0c766618f97e1
ItalloMoreno22 <Itallorodrigues2628@gmail.com>
2022-11-29 11:09:40 -0300

    linkagem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DogzComponent } from './DOG/dogz.component';
 import { DOGComponent } from './dog/dog.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DogzComponent,
     DOGComponent
   ],
   imports: [
--- src/app/message.ts
+++ src/app/message.ts
@@ -1,4 +1,4 @@
 export interface Messa {
   message:string;
-  status: string;
+
 }
